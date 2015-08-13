6e970312df5fa4e184646aa30f4ec5ab7c9e0980
jpv <thevirus4@hotmail.com>
2015-08-13 14:42:19 +0200

    Refactor adapter to use Cursor instead of a List of items

--- app/src/main/java/es/jpv/android/examples/toolbarcabexample/dummy/DummyContent.java
+++ app/src/main/java/es/jpv/android/examples/toolbarcabexample/dummy/DummyContent.java
@@ -1,8 +1,9 @@
 package es.jpv.android.examples.toolbarcabexample.dummy;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -16,85 +17,38 @@ public class DummyContent {
     /**
      * An array of sample (dummy) items.
      */
-    public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
-
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
-    public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+    public static Map<String, String> ITEMS = new LinkedHashMap<String, String>();
+    public static MatrixCursor ITEMS_CURSOR;
 
     static {
-        // Add 24 sample items.
-        addItem(new DummyItem("1", "Item 1"));
-        addItem(new DummyItem("2", "Item 2"));
-        addItem(new DummyItem("3", "Item 3"));
-        addItem(new DummyItem("4", "Item 4"));
-        addItem(new DummyItem("5", "Item 5"));
-        addItem(new DummyItem("6", "Item 6"));
-        addItem(new DummyItem("7", "Item 7"));
-        addItem(new DummyItem("8", "Item 8"));
-        addItem(new DummyItem("9", "Item 9"));
-        addItem(new DummyItem("10", "Item 10"));
-        addItem(new DummyItem("11", "Item 11"));
-        addItem(new DummyItem("12", "Item 12"));
-        addItem(new DummyItem("13", "Item 13"));
-        addItem(new DummyItem("14", "Item 14"));
-        addItem(new DummyItem("15", "Item 15"));
-        addItem(new DummyItem("16", "Item 16"));
-        addItem(new DummyItem("17", "Item 17"));
-        addItem(new DummyItem("18", "Item 18"));
-        addItem(new DummyItem("19", "Item 19"));
-        addItem(new DummyItem("20", "Item 20"));
-        addItem(new DummyItem("21", "Item 21"));
-        addItem(new DummyItem("22", "Item 22"));
-        addItem(new DummyItem("23", "Item 23"));
-        addItem(new DummyItem("24", "Item 24"));
+        //Create root items
+        for (int i=0 ; i<30 ; i++) {
+            ITEMS.put(String.valueOf(i), "Item " + (i));
+        }
+        // Add 30 sample items to the MatrixCursor
+        buildCursor();
     }
 
-    public static void addItem(DummyItem item) {
-        ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
+    public static Cursor buildCursor() {
+        ITEMS_CURSOR = new MatrixCursor(new String[] {"_id", "value"});
+        for (Map.Entry<String, String> entry : ITEMS.entrySet()) {
+            ITEMS_CURSOR.addRow(new String[] {entry.getKey(), entry.getValue()});
+        }
+        return ITEMS_CURSOR;
     }
 
-    public static void deleteItem(int position) {
-        DummyItem itemToDelete = null;
-        position++;
-        for (DummyItem item : ITEMS) {
-            if (item.id.equals(String.valueOf(position))) {
-                itemToDelete = item;
-                break;
-            }
-        }
-        if (itemToDelete != null) {
-            ITEMS.remove(itemToDelete);
-            itemToDelete = null;
-        }
-        for (DummyItem item : ITEM_MAP.values()) {
-            if (item.id.equals(String.valueOf(position))) {
-                itemToDelete = item;
-                break;
-            }
-        }
-        if (itemToDelete != null) {
-            ITEM_MAP.remove(itemToDelete);
-        }
+    public static Cursor addItem(Integer key) {
+        ITEMS.put(String.valueOf(key), "Item " + key);
+        return buildCursor();
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
-    public static class DummyItem {
-        public String id;
-        public String content;
-
-        public DummyItem(String id, String content) {
-            this.id = id;
-            this.content = content;
+    public static Cursor deleteItem(int position) {
+        ITEMS_CURSOR.moveToPosition(position);
+        String keyToDelete = ITEMS_CURSOR.getString(0); //ID
+        if (keyToDelete != null) {
+            ITEMS.remove(keyToDelete);
         }
-
-        @Override
-        public String toString() {
-            return content;
-        }
+        return buildCursor();
     }
+
 }
